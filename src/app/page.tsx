18ba4f3963033027b3638a67897fa7dbf92684b6
Ken Sakuda <ken_sakuda@b-mystory.com>
2025-10-10 23:59:09 +0900

    v0.47commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -278,6 +278,59 @@
 //   );
 // }
 
+// import styles from "./page.module.css";
+// import Layout from "./_components/Layout";
+// import Main from "./_components/Main";
+// import Sub from "./_components/Sub";
+// import Ad from "./_components/Ad";
+// import RecruitBanner from "./_components/RecruitBanner";
+// import GlossaryBanner from "./_components/GlossaryBanner";
+// import { TOP_ARTICLE_LIST_LIMIT } from "@/app/_constants";
+// import { getArticleList } from "@/app/_libs/microcms";
+// import Cards from "./_components/Cards";
+// import Ranking from "./_components/Ranking";
+// import SearchField from "./_components/SearchField";
+// import ButtonLink from "./_components/ButtonLink";
+// // import Predict from "./_components/Predict";
+// import ScrollToTop from "./_components/ScrollToTop";
+
+// export const revalidate = 60;
+
+// type Props = {
+//   searchParams: Promise<{
+//     rankingDraftKey?: string;
+//   }>;
+// };
+
+// export default async function Page({ searchParams }: Props) {
+//   const resolvedSearchParams = await searchParams;
+//   const data = await getArticleList({ limit: TOP_ARTICLE_LIST_LIMIT });
+//   return (
+//     <Layout>
+//       <div className={styles.container}>
+//         <Main className={styles.mainContent}>
+//           <div className={styles.cards}>
+//             <Cards articles={data.contents} />
+//           </div>
+//           <div className={styles.buttonLink}>
+//             <ButtonLink href="/articles">Read More</ButtonLink>
+//           </div>
+//         </Main>
+//         <Sub className={styles.sidebar}>
+//           <Ad />
+//           <Ranking draftKey={resolvedSearchParams.rankingDraftKey} />
+//           <GlossaryBanner />
+//           {/* <Predict /> */}
+//           <SearchField />
+//           <RecruitBanner />
+//           <Ad />
+//         </Sub>
+//       </div>
+//       <ScrollToTop />
+//     </Layout>
+//   );
+// }
+
 import styles from "./page.module.css";
 import Layout from "./_components/Layout";
 import Main from "./_components/Main";
@@ -291,7 +344,6 @@ import Cards from "./_components/Cards";
 import Ranking from "./_components/Ranking";
 import SearchField from "./_components/SearchField";
 import ButtonLink from "./_components/ButtonLink";
-// import Predict from "./_components/Predict";
 import ScrollToTop from "./_components/ScrollToTop";
 
 export const revalidate = 60;
@@ -305,6 +357,7 @@ type Props = {
 export default async function Page({ searchParams }: Props) {
   const resolvedSearchParams = await searchParams;
   const data = await getArticleList({ limit: TOP_ARTICLE_LIST_LIMIT });
+
   return (
     <Layout>
       <div className={styles.container}>
@@ -316,14 +369,17 @@ export default async function Page({ searchParams }: Props) {
             <ButtonLink href="/articles">Read More</ButtonLink>
           </div>
         </Main>
+
+        {/* ▼ ここを mobileEdge で包む */}
         <Sub className={styles.sidebar}>
-          <Ad />
-          <Ranking draftKey={resolvedSearchParams.rankingDraftKey} />
-          <GlossaryBanner />
-          {/* <Predict /> */}
-          <SearchField />
-          <RecruitBanner />
-          <Ad />
+          <div className={styles.mobileEdge}>
+            <Ad />
+            <Ranking draftKey={resolvedSearchParams.rankingDraftKey} />
+            <GlossaryBanner />
+            <SearchField />
+            <RecruitBanner />
+            <Ad />
+          </div>
         </Sub>
       </div>
       <ScrollToTop />
